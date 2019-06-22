704fcfdd2160221e209ea9a820eb00fb0f176e79
peter <peter@unnotech.com>
2019-06-22 18:06:56 +0800

    fix broker msg format

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM golang:1.12.5
 
-WORKDIR /usr/src/app
-COPY . /usr/src/app/.
+RUN curl -fsSL -o /usr/local/bin/dep https://github.com/golang/dep/releases/download/v0.5.3/dep-linux-amd64 && chmod +x /usr/local/bin/dep
 
-ENTRYPOINT ["/usr/src/app/entrypoint.sh"]
+WORKDIR /go/src/kumo
+COPY . /go/src/kumo/.
+COPY kumo/Gopkg.toml kumo/Gopkg.lock ./
 
-EXPOSE 8080
+RUN dep ensure -vendor-only
+
+ENTRYPOINT [ "make", "dev" ]
+
+EXPOSE 8001
--- services/tasks.go
+++ services/tasks.go
@@ -1,16 +1,17 @@
 package services
 
 import (
-	"os"
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
-	"time"
 	"kumo/httpd/handler/stock"
-	"github.com/jinzhu/gorm"
 	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/jinzhu/gorm"
 
 	"github.com/streadway/amqp"
 	// "github.com/RichardKnop/machinery/v1"
@@ -54,7 +55,7 @@ func StockCrawler() error {
 }
 
 func GetData(code string, db *gorm.DB) error {
-	BroadcastMessage("kumo_broadcast", "yayayay!!message from kumo~~~~~")
+	// BroadcastMessage("kumo_broadcast", "yayayay!!message from kumo~~~~~")
 
 	// get response
 	resp, err := http.Get("https://tw.quote.finance.yahoo.net/quote/q?type=tick&perd=1m&mkt=10&sym=" + code)
@@ -98,8 +99,8 @@ func GetData(code string, db *gorm.DB) error {
 	// Get or Create the Product from db
 	var product stock.Product
 	err = db.Where("Code = ?", id).First(&product).Error
-	if gorm.IsRecordNotFoundError(err){
-		product = stock.Product {Code: id,}
+	if gorm.IsRecordNotFoundError(err) {
+		product = stock.Product{Code: id}
 		db.Create(&product)
 	}
 	// fmt.Println("product: ", product)
@@ -117,30 +118,44 @@ func GetData(code string, db *gorm.DB) error {
 		err := db.Where("time = ?", tickTime).Where("product_id = ?", product.ID).First(&priceObj).Error
 		if err == nil {
 			// duplicated result, skip
-			continue
-		} else if !gorm.IsRecordNotFoundError(err){
+			// continue
+		} else if !gorm.IsRecordNotFoundError(err) {
 			fmt.Println("get price error: ", err.Error())
-			return err
+			// return err
 		}
 
 		// Prepare the price object
 		priceObj = stock.Price{
-			Product:   product,
-			Time:      tickTime,
-			Price:     price,
-			Volume:    uint(volume),
+			Product: product,
+			Time:    tickTime,
+			Price:   price,
+			Volume:  uint(volume),
 		}
 		//save the Price object
 		if err := db.Set("gorm:save_associations", false).Create(&priceObj).Error; err != nil {
 			fmt.Println("create price error: ", err.Error())
-			return err
+			// return err
 		}
-		
-		totalSaveCount++
-	}
 
-	// TODO: push the new price info to broker
-	BroadcastMessage("kumo_broadcast", "get data!!!!message from kumo~~~~~")
+		totalSaveCount++
+		// TODO: push the new price info to broker
+		msg := make(map[string]interface{})
+		msg["product_id"] = priceObj.Product.ID
+		msg["time"] = priceObj.Time
+		msg["price"] = priceObj.Price
+		msg["volume"] = priceObj.Volume
+
+		b, err := json.Marshal(msg)
+		if err != nil {
+			fmt.Print("parse price object into json error", err.Error())
+		}
+		BroadcastMessage("kumo_broadcast", b)
+
+		// var p stock.Price
+		// json.Unmarshal(b, &p)
+		fmt.Println(b)
+		// fmt.Println(p.Price)
+	}
 
 	fmt.Println(mkt, id, mem)
 	fmt.Println("totalSaveCount: ", totalSaveCount)
@@ -166,7 +181,7 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func BroadcastMessage(exchange string, msg string) {
+func BroadcastMessage(exchange string, msg []byte) {
 	rabbitmqHost := os.Getenv("RABBITMQ_HOST")
 	rabbitmqUser := os.Getenv("RABBITMQ_DEFAULT_USER")
 	rabbitmqPass := os.Getenv("RABBITMQ_DEFAULT_PASS")
@@ -181,26 +196,26 @@ func BroadcastMessage(exchange string, msg string) {
 	defer ch.Close()
 
 	err = ch.ExchangeDeclare(
-		exchange,   // name
-		"fanout",     // type
-		true,         // durable
-		false,        // auto-deleted
-		false,        // internal
-		false,        // no-wait
-		nil,          // arguments
+		exchange, // name
+		"fanout", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
 	)
 	failOnError(err, "Failed to declare an exchange")
 
 	// body := bodyFrom(os.Args)
 	// body := "broadcast msg ~~~ ~~~"
 	err = ch.Publish(
-		exchange,   // exchange
-		"",           // routing key
-		false,        // mandatory
-		false,        // immediate
+		exchange, // exchange
+		"",       // routing key
+		false,    // mandatory
+		false,    // immediate
 		amqp.Publishing{
 			ContentType: "text/plain",
-			Body:        []byte(msg),
+			Body:        msg,
 		})
 	failOnError(err, "Failed to publish a message")
 
